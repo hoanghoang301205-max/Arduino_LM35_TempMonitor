a13c181cc07c2b117e593996635f3463af6ca5f1
taminhquan-iot <taminhquansg@gmail.com>
2026-04-14 14:55:11 +0700

    Add LM35 code

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,1 +1,16 @@
-void setup(){} void loop(){}
+int adcValue;
+float nhietDo;
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+}
+
+void loop() {
+  adcValue = analogRead(A0);
+  nhietDo = (adcValue * 500.0) / 1023.0;
+  Serial.println(nhietDo, 1);
+  delay(1000);
+}
+
+
